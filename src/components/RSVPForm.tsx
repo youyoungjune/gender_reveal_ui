ed99e5ba95a4f8aadf0870e36e29fadab300a876
You Young June <youngjune@planorban.com>
2024-11-06 20:18:47 -0500

    landing page pretty much done

--- src/components/RSVPForm.tsx
+++ src/components/RSVPForm.tsx
@@ -9,25 +9,29 @@ export const RSVPForm = () => {
   const [visiblePassword, setVisiblePassword] = useState(false);
   return (
     <div className="p-4 space-y-4">
-      <Input placeholder="First and Last Name" />
+      <Input
+        placeholder="First and Last Name"
+        className="text-[#954f36] bg-[#fef6ed]"
+      />
       <div className="relative">
         <Input
           placeholder="Password"
           type={visiblePassword ? "text" : "password"}
+          className="text-[#954f36] bg-[#fef6ed]"
         />
         {visiblePassword ? (
           <EyeIcon
-            className="absolute right-4 top-2 z-10 cursor-pointer text-gray-500"
+            className="absolute right-4 top-2 z-10 cursor-pointer text-[#954f36]"
             onClick={() => setVisiblePassword(false)}
           />
         ) : (
           <EyeClosedIcon
-            className="absolute right-4 top-2 z-10 cursor-pointer text-gray-500"
+            className="absolute right-4 top-2 z-10 cursor-pointer text-[#954f36]"
             onClick={() => setVisiblePassword(true)}
           />
         )}
       </div>
-      <Button className="w-full bg-violet-950 hover:bg-violet-900">
+      <Button className="w-full bg-[#bed0d8] hover:bg-[#bad5c6] text-[#fef6ed]">
         Enter
       </Button>
     </div>
